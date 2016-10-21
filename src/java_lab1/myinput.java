188c17d1704ce8b623e615251a5839ead55df98e
CP-D <duchenpeng@139.com>
2016-10-21 23:28:16 +0800

    modify for checkstyle and findbugs by Sida Wei

--- src/java_lab1/myinput.java
+++ src/java_lab1/myinput.java
@@ -1,24 +1,22 @@
-package java_lab1;
+package javalab;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class myinput 
-{
-	public static String read_string()
-	{
-		BufferedReader br=new BufferedReader( new InputStreamReader(System.in),1);
-		String string="";
-		try {
-			string = br.readLine();
-		} catch (IOException ex) 
-		{
-			System.out.println(ex);
-		}
-		return string;
-	}
-	
-	public static int read_int()
-	{
-		return Integer.parseInt(read_string());
-	}
+public class myinput {
+    public static String read_string() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1);
+        String string = "";
+        try {
+            string = br.readLine();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        return string;
+    }
+
+    public static int read_int() {
+        return Integer.parseInt(read_string());
+    }
 }
